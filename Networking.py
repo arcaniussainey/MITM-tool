7d059d77782e9560f5f0604f30dd1bc37ce9c711
brian <br.3cow@gmail.com>
2018-12-24 16:37:17 -0500

    added more networking

--- Networking.py
+++ Networking.py
@@ -14,18 +14,30 @@ PctCnt = 0;
 nps = nmap.PortScanner()
 
 # packet parsers
-def AddrP(packet):
+def PathP(packet):
+    # gets the packets src and destination
     PctCnt += 1
-    print(f"Packet #{PctCnt} @{packet[0][1].src} ==> {packet[0][1].dst} ")
+    print(f"Packet #{PctCnt} @{packet[0][1].src} ―→ {packet[0][1].dst} ")
   #
 def PacketPrint(in):
+    # colors a packet
     cprint(str(in), 'blue', attrs=["bold"]) # print in blue
   #
 # settings
 
 # scanner types
+  # the target is the ip, the ports are the range of ports as such 34-40, or 22-181
 def individualScan(targ, ports):
-    ScanData = nps.scan(str(targ), str(ports))
+    SD = nps.scan(str(targ), str(ports))
+    # get the state of the targeted device, the open ports, vendor, etc
+    state = SD['scan']['']
+    return """
+
+
+           """
+  #
+def CheckOnline():
+    nps.scan(arguments='-pn')
 # sniffer types
   #
 def RawSniff(f=""):
